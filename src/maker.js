543cc96f6e2f3ef9190ce2f34f16466bdd117e9d
Mike Meadows <mmeadow33@gmail.com>
2016-07-03 16:24:10 -0500

    added all needed files

---
+++ src/maker.js
@@ -0,0 +1,19 @@
+"use strict";
+
+const Bread = require('./bread');
+const Cheese = require('./cheese');
+const Meat = require('./meat');
+const Veggies = require('./veggies');
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = Maker; 
